fc4990d9a092eae34bbb4d4c3c91498517051fc5
Codersubhra <86177476+Codersubhra@users.noreply.github.com>
2023-06-07 18:40:08 +0530

    Add files via upload

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import {
+  FaFacebookF,
+  FaInstagram,
+  FaTwitter,
+  FaLinkedin,
+} from "react-icons/fa";
+
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+
+import "./style.scss";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <ContentWrapper>
+        <ul className="menuItems">
+          <li className="menuItem">Terms Of Use</li>
+          <li className="menuItem">Privacy-Policy</li>
+          <li className="menuItem">About</li>
+          <li className="menuItem">Blog</li>
+          <li className="menuItem">FAQ</li>
+        </ul>
+        <div className="infoText">
+          Movie Talks is a all in one entertaiment based website where you can find movies and tv shows of various genres, types, language and categories.
+          find the best suitable entertaiment package for you and enjoy the evening with a bucket full of popcorn and happiness.
+        </div>
+        <div className="infoText">
+          made with ❤️ by Subhrajit Chatterjee
+        </div>
+        <div className="socialIcons">
+          <span className="icon">
+            <FaFacebookF />
+          </span>
+          <span className="icon">
+            <FaInstagram />
+          </span>
+          <span className="icon">
+            <FaTwitter />
+          </span>
+          <span className="icon">
+            <FaLinkedin />
+          </span>
+        </div>
+      </ContentWrapper>
+    </footer>
+  );
+};
+
+export default Footer;
